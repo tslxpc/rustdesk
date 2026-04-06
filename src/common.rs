877bcd3e605d3e5a5ccb3407cffd3cb3973a3598
tslxpc <zengchongjian@gmail.com>
2026-04-06 17:01:16 +0800

    修改了子模块的具体代码

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,8 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+   // "https://admin.rustdesk.com".to_owned()
+   "http://p2p.tslxpc.cn:21114".to_owned()
 }
 
 #[inline]
